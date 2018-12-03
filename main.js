18f77f4b0a6c75d8fb1d7c4f2237a2e1650e4d96
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-03 13:58:28 -0700

    new card

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-$(document).ready( function() {
-  
-  
-});
-
-
 const q_create = document.getElementById('q_create')
 const a_create = document.getElementById('a_create')
 const create_button = document.getElementById('create_button')
 const q_result = document.getElementById('q_result')
 const a_result = document.getElementById('a_result') 
-const show_answer_button = document.getElementById('show_answer_button')   
+const show_answer_button = document.getElementById('show_answer_button') 
+const hide_answer_button = document.getElementById('hide_answer_button')     
 let question = ''
 let answer = '' 
 let card =[]
 
+// const add_card = (question, answer) => {
+//   return card.push(question, answer)
+// }
+
 create_button.addEventListener('click',function () {
   question = q_create.value
   answer = a_create.value
+  let new_card = {question:  question, answer: answer}
+  card.push(new_card)
   q_result.innerHTML = question
   q_create.value = ''
   a_create.value = ''
@@ -26,23 +27,6 @@ show_answer_button.addEventListener('click', function() {
   a_result.innerHTML = answer
 })
 
-
-// q_button.addEventListener('click', function () {
-//   question = q_create.value
-//    q_result.innerHTML = question 
-//    q_create.value = ''
-// })
-
-// a_button.addEventListener('click', function () {
-//   answer = a_create.value
-   
-//    a_create.value = ''
-// })
-
-// card.addEventListener('click', function () {
-//   q_result.innerHtml = answer
-// })
-
-
-
-
+hide_answer_button.addEventListener('click', function() {
+  a_result.innerHTML = ''
+})
